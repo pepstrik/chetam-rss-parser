976824930325f9f36b75b5ed0e789fb0a5f5c665
Katya Glukhova <116115558+pepstrik@users.noreply.github.com>
2025-04-21 17:57:22 +0300

    Update rss_to_json.py

--- rss_to_json.py
+++ rss_to_json.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import feedparser
 import json
+import re
 
 # Скачиваем RSS
 rss_url = "https://st.zvuk.com/r/c8908758-89a7-431e-90bf-bb0f4c80bc97/rss.xml"
@@ -12,12 +13,14 @@ for entry in feed.entries:
     pub_date = entry.get('published', '')
     year = None
 
-    # Проверяем, есть ли год в поле 'published' и правильно его извлекаем
+    # Проверяем, есть ли год в поле 'published' и правильно его извлекаем с использованием регулярных выражений
     if pub_date:
-        try:
-            year = datetime.strptime(pub_date, "%a, %d %b %Y %H:%M:%S %z").year
-        except Exception:
-            # Если год не извлекается, используем текущий
+        # Применяем регулярное выражение для извлечения года
+        match = re.search(r'(\d{4})', pub_date)
+        if match:
+            year = match.group(1)
+        else:
+            # Если год не удалось извлечь, используем текущий
             year = datetime.now().year
 
     episodes_data.append({
